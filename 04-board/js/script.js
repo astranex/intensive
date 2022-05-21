983e09edd4a15b886b89dd5e132eb7de7305f51c
Галюзин Виталий Юрьевич <v.galyuzin@lugacom.local>
2022-05-21 08:55:03 +0300

    made my homework: changed the background, modified the array of colors, added randomization of the board size

--- 04-board/js/script.js
+++ 04-board/js/script.js
@@ -1,7 +1,8 @@
 
 const board = document.querySelector('#board')
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const PIXELS_COUNT = 500
+const colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '4B0082', '9400D3'] // Массив цветов радуги
+const sizes = [300, 400, 500, 600, 700] // Массив размеров
+const PIXELS_COUNT = sizes[Math.floor(Math.random() * sizes.length)] // Рандомно определяем размер доски
 
 for (let i = 0; i < PIXELS_COUNT; i++) {
    const pixel = document.createElement('div') // Создаём элементы (пиксели) пока цикл не дойдёт до PIXEL_COUNT
